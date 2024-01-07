31191507be9949fe79029850e947f2d577802069
Shay Yzhakov <shayy@nonamesecurity.com>
2024-01-07 23:26:14 +0200

    extract add soldier modal

---
+++ frontend/src/components/modals/AddSoldierModal.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { type CreateSoldierParams, createSoldier } from '@/apis';
+import { ElNotification } from 'element-plus';
+import { useSoldiersStore } from '@/stores/soldiers.store';
+
+const showCreateSoldierModal = defineModel('showModal');
+
+const soldiersStore = useSoldiersStore();
+
+const newSoldierParams = reactive<CreateSoldierParams>({
+  first_name: '',
+  last_name: '',
+  personal_number: '',
+  phone_number: '',
+  capabilities: [],
+});
+
+function resetNewSoldierParams() {
+  newSoldierParams.first_name = '';
+  newSoldierParams.last_name = '';
+  newSoldierParams.personal_number = '';
+  newSoldierParams.phone_number = '';
+  newSoldierParams.capabilities = [];
+}
+
+async function saveNewSoldier() {
+  try {
+    await createSoldier(newSoldierParams);
+
+    ElNotification({
+      message: 'Soldier created successfully',
+      type: 'success',
+    });
+  } catch (e) {
+    ElNotification({
+      title: 'Action failed',
+      message: 'Failed to create a new soldier',
+      type: 'error',
+    });
+  } finally {
+    showCreateSoldierModal.value = false;
+    resetNewSoldierParams();
+    soldiersStore.refreshSoldiers();
+  }
+}
+</script>
+
+<template>
+  <el-dialog v-model="showCreateSoldierModal" title="New Soldier" width="500px">
+    <el-form :model="newSoldierParams" label-width="180px" label-position="left">
+      <el-form-item label="Soldier First Name">
+        <el-input v-model="newSoldierParams.first_name" />
+      </el-form-item>
+
+      <el-form-item label="Soldier Last Name">
+        <el-input v-model="newSoldierParams.last_name" />
+      </el-form-item>
+
+      <el-form-item label="Soldier Personal Number">
+        <el-input v-model="newSoldierParams.personal_number" />
+      </el-form-item>
+
+      <el-form-item label="Soldier Phone Number">
+        <el-input v-model="newSoldierParams.phone_number" />
+      </el-form-item>
+
+      <!-- TODO: capabilities -->
+    </el-form>
+
+    <template #footer>
+      <el-button @click="showCreateSoldierModal = false">Cancel</el-button>
+      <el-button type="primary" @click="saveNewSoldier">Save</el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<style scoped></style>
--- frontend/src/views/SoldiersManagementView.vue
+++ frontend/src/views/SoldiersManagementView.vue
@@ -6,13 +6,12 @@ import {
   type GuardPost,
   type Team,
   type UpdateTeamParams,
-  type CreateSoldierParams,
-  createSoldier,
   deleteSoldier,
 } from '@/apis';
 import { ElNotification } from 'element-plus';
 import { useTeamsStore } from '@/stores/teams.store';
 import { useSoldiersStore } from '@/stores/soldiers.store';
+import AddSoldierModal from '@/components/modals/AddSoldierModal.vue';
 
 const teamsStore = useTeamsStore();
 const soldiersStore = useSoldiersStore();
@@ -30,14 +29,6 @@ const selectedTeamParams = reactive<UpdateTeamParams>({
   name: '',
   people: [],
   guardPosts: [],
-});
-
-const newSoldierParams = reactive<CreateSoldierParams>({
-  first_name: '',
-  last_name: '',
-  personal_number: '',
-  phone_number: '',
-  capabilities: [],
 });
 
 const guardPosts = ref<GuardPost[]>();
@@ -96,35 +87,6 @@ async function saveTeamChanges() {
   } finally {
     showEditTeamModal.value = false;
     teamsStore.refreshTeams();
-  }
-}
-
-function resetNewSoldierParams() {
-  newSoldierParams.first_name = '';
-  newSoldierParams.last_name = '';
-  newSoldierParams.personal_number = '';
-  newSoldierParams.phone_number = '';
-  newSoldierParams.capabilities = [];
-}
-
-async function saveNewSoldier() {
-  try {
-    await createSoldier(newSoldierParams);
-
-    ElNotification({
-      message: 'Soldier created successfully',
-      type: 'success',
-    });
-  } catch (e) {
-    ElNotification({
-      title: 'Action failed',
-      message: 'Failed to create a new soldier',
-      type: 'error',
-    });
-  } finally {
-    showCreateSoldierModal.value = false;
-    resetNewSoldierParams();
-    soldiersStore.refreshSoldiers();
   }
 }
 
@@ -285,34 +247,7 @@ async function removeSoldierByIndex(index: number) {
       </template>
     </el-dialog>
 
-    <!-- NEW SOLDIER -->
-    <!-- TODO: extract to component -->
-    <el-dialog v-model="showCreateSoldierModal" title="New Soldier" width="500px">
-      <el-form :model="newSoldierParams" label-width="180px" label-position="left">
-        <el-form-item label="Soldier First Name">
-          <el-input v-model="newSoldierParams.first_name" />
-        </el-form-item>
-
-        <el-form-item label="Soldier Last Name">
-          <el-input v-model="newSoldierParams.last_name" />
-        </el-form-item>
-
-        <el-form-item label="Soldier Personal Number">
-          <el-input v-model="newSoldierParams.personal_number" />
-        </el-form-item>
-
-        <el-form-item label="Soldier Phone Number">
-          <el-input v-model="newSoldierParams.phone_number" />
-        </el-form-item>
-
-        <!-- TODO: capabilities -->
-      </el-form>
-
-      <template #footer>
-        <el-button @click="showCreateSoldierModal = false">Cancel</el-button>
-        <el-button type="primary" @click="saveNewSoldier">Save</el-button>
-      </template>
-    </el-dialog>
+    <AddSoldierModal v-model:showModal="showCreateSoldierModal" />
   </div>
 </template>
 
