a9354e52fe8903a88bc34fc0ef9c39f2a5076ef9
Shay Yzhakov <shayy@nonamesecurity.com>
2024-01-20 20:54:28 +0200

    improve guard post description

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -11,10 +11,12 @@ export enum StrategyType {
 export const strategies: Record<StrategyType, Strategy> = {
   roundrobin: {
     name: 'Round-Robin',
-    description: 'Each soldier guards in turn, one after the other',
+    description:
+      'Each soldier guards in turn, one after the other. Best fits routine guards, e.g 4-8',
   },
   'team-roundrobin': {
     name: 'Team Round-Robin',
-    description: 'Each team guards in turn, one after the other',
+    description:
+      'Each team guards in turn, one after the other. Best fits routine team tasks, e.g daily Konenut',
   },
 };
